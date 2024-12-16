2ebb8b0fc3f87fb77984636ca33b8e79654ed11d
Curt Miller <millercw3@gmail.com>
2024-12-16 13:55:45 -0500

    submit

---
+++ index.js
@@ -0,0 +1,106 @@
+require("dotenv").config();
+const pg = require("pg");
+const client = new pg.Client(process.env.DATABASE_URL);
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+app.use(require("morgan")("dev"));
+
+// routes below:
+// CREATE
+app.post('/api/flavors', async (req, res, next) => {
+    try {
+        const SQL = `
+        INSERT INTO flavors(name, is_favorite)
+        VALUES($1, $2)
+        RETURNING * 
+        `;
+        const response = await client.query(SQL, [req.body.name, req.body.is_favorite]);
+        res.send(response.rows[0]);
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+// READ
+app.get('/api/flavors', async (req, res, next) => {
+    try {
+        const SQL = `SELECT * FROM flavors ORDER BY id ASC;`;
+        const response = await client.query(SQL);
+        res.send(response.rows);
+    } catch (error) {
+        next(error);
+    }
+});
+
+// READ - single flavor
+app.get('/api/flavors/:id', async (req, res, next) => {
+    try {
+        const SQL = `SELECT * FROM flavors WHERE id = $1;`;
+        const response = await client.query(SQL, [req.params.id]);
+        res.send(response.rows[0]);
+    } catch (error) {
+        next(error);
+    }
+});
+
+// UPDATE
+app.put('/api/flavors/:id', async (req, res, next) => {
+    try {
+        const SQL = `
+        UPDATE flavors
+        SET name = $1, is_favorite = $2
+        WHERE id = $3
+        RETURNING *;
+      `;
+        const response = await client.query(SQL, [
+            req.body.name,
+            req.body.is_favorite,
+            req.params.id,
+        ]);
+        res.send(response.rows[0]);
+    } catch (error) {
+        next(error);
+    }
+});
+
+// DELETE
+app.delete('/api/flavors/:id', async (req, res, next) => {
+    try {
+        const SQL = `DELETE FROM flavors WHERE id = $1;`;
+        await client.query(SQL, [req.params.id]);
+        res.sendStatus(204); // No Content
+    } catch (error) {
+        next(error);
+    }
+});
+
+const init = async () => {
+    await client.connect();
+
+    let SQL = `
+        DROP TABLE IF EXISTS flavors;
+        CREATE TABLE flavors (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        is_favorite BOOLEAN DEFAULT false
+        );
+        `;
+    await client.query(SQL);
+    console.log('table created');
+
+    SQL = `
+        INSERT INTO flavors(name, is_favorite) VALUES('Vanilla', true);
+        INSERT INTO flavors(name, is_favorite) VALUES('Chocolate', false);
+        INSERT INTO flavors(name, is_favorite) VALUES('Strawberry', true);
+        `;
+    await client.query(SQL);
+    console.log('data seeded');
+
+    const port = process.env.PORT;
+    app.listen(port, () => console.log(`listening on port ${port}`));
+};
+
+init();
